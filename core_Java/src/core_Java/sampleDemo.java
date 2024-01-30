2c1cf577bfb169b1d19747e401d54037efe053b4
Admin <Admin@DESKTOP-TC4OO1O>
2024-01-30 11:09:20 +0530

    First commit 30-1-2024

---
+++ core_Java/src/core_Java/sampleDemo.java
@@ -0,0 +1,11 @@
+package core_Java;
+
+public class sampleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Eclipse into github");
+
+	}
+
+}
